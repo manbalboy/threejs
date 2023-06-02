a41807548e6e36626da343652ca8025da6e6a96e
manbalboy <manbalboy@hanmail.net>
2023-06-03 08:19:21 +0900

    first commit

---
+++ custom_model_fin/src/ex03.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+// ----- 주제: glb 애니메이션
+
+export default 
